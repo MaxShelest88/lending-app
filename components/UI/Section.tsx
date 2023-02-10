44176732bfad5c1e0707396a3cd70f62a5ccdf7b
Maksim Shelest <shelest.ks@gmail.com>
2023-02-10 15:38:29 +0300

    disabled full screen

--- components/UI/Section.tsx
+++ components/UI/Section.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Section: React.FC<Props> = ({ id, children }) => {
   return (
     <>
-      <section className="section relative py-20 px-5" id={id}>
+      <section className="relative py-20 px-5" id={id}>
         <div className="container mx-auto">{children}</div>
         <div className="absolute bottom-0 left-0 z-10 block h-[1px] w-full bg-gray-200/80" />
       </section>
