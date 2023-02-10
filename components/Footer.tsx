6232cd794d3b73e2aeb3c47b6217dadd45190966
Maksim Shelest <shelest.ks@gmail.com>
2023-02-10 15:22:12 +0300

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <>
       <footer className="text-zinc-800 dark:bg-neutral-800 dark:text-white">
-        <div className="container mx-auto flex h-[80px] items-center justify-between py-2 px-0 xl:px-5">
+        <div className="container mx-auto flex h-[80px] items-center justify-between py-2 px-5 md:px-0">
           <p>Copyright &copy; {new Date().getFullYear()} Maksim Shelest</p>
         </div>
       </footer>
